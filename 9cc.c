4cd9365b5a701ab2ba06f38d2583d420187125b2
Heechan Park <phcphc82@gmail.com>
2020-04-30 11:50:03 +0900

    Step 2

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,28 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf("  .global main\n");
   printf("main:\n");
-  printf("  mov r0, #%d\n", atoi(argv[1]));
+  printf("  mov r0, #%d\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      ++p;
+      printf("  add r0, #%d\n", strtol(p, &p, 10));
+      continue;
+    }
+    if (*p == '-') {
+      ++p;
+      printf("  sub r0, #%d\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "undefined operator '%c'\n", *p);
+    return 1;
+  }
+
   printf("  bx lr\n");
   return 0;
 }
-
